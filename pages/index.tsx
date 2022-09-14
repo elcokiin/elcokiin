c8ca3b8d6886728cfca3ff14f1dbaa880e838149
elcokiin <tenjodiego961@gmail.com>
2022-09-14 02:06:57 -0500

    change image in the tab of window

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Inicio - elcokiin</title>
         <meta name="description" content="Place to write and read other people" />
-        <link rel="icon" href="/mouse-dark.png" />
+        <link rel="icon" href="/mouse-light.png" />
 			</Head>
 
 			<NavBarMobile />
@@ -30,15 +30,7 @@ const Home: NextPage = () => {
 				<Categories />
 			</header>
 
-			<main>
-				<h1>Home</h1>
-				<h3>
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).	
-				</h3>
-			</main>
+			<main></main>
 
 
 			<footer></footer>
